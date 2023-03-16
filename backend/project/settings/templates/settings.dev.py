5b53f06108e404a4357bf4897af7c4c4b20ee268
PiyushVyas13 <jaiprakashvyas6@gmail.com>
2023-03-17 01:14:07 +0530

    Initial Commit

---
+++ backend/project/settings/templates/settings.dev.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-j6&r%=_vtj%4mkj9d$4a9jg1ot*0+-ntl1*5ep4=y4e0$n8g*l"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
